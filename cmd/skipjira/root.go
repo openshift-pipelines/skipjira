204b82b43992dfbd1f8109a6358798098a58728e
Akshay Pant <akshay.akshaypant@gmail.com>
2026-03-13 09:07:31 +0530

    chore: initialise project
    Signed-off-by: Akshay Pant <akshay.akshaypant@gmail.com>

---
+++ cmd/skipjira/root.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "skipjira",
+	Short: "Link GitHub PRs to Jira tickets",
+	Long: `SkipJira automatically links GitHub Pull Requests to Jira tickets.
+
+It installs a git pre-push hook that prompts for Jira ticket IDs when pushing
+new branches, then batch-updates Jira tickets via a cron job.`,
+	SilenceUsage:  true,
+	SilenceErrors: true,
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+	rootCmd.AddCommand(installCmd)
+	rootCmd.AddCommand(queueCmd)
+	rootCmd.AddCommand(syncCmd)
+	rootCmd.AddCommand(statusCmd)
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/theakshaypant/skipjira
+
+go 1.25.5
+
+require (
+	github.com/andygrunwald/go-jira v1.17.0
+	github.com/google/go-github/v81 v81.0.0
+	github.com/spf13/cobra v1.10.2
+	golang.org/x/oauth2 v0.35.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/pflag v1.0.9 // indirect
+	github.com/trivago/tgo v1.0.7 // indirect
+)
